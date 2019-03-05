ca75b638edb590bcaf2cb9f92b5a6c505b6887ed
wiktor12v <wiktor1230@op.pl>
2019-03-05 15:44:52 +0100

    komit3

--- osoby/views.py
+++ osoby/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1>witaj w django!</h1>")
+def test(request):
+    return HttpResponse("<h1>sprawdzian z matmy jutro</h1>")
